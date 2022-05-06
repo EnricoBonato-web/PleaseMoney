5f785312822bbd7e9e190327ee2654570f700502
Enrico <enrico.bonato95@gmail.com>
2022-05-06 10:54:51 +0200

    fixed javascript

--- js/scripts.js
+++ js/scripts.js
@@ -2,13 +2,22 @@ var coll = document.getElementsByClassName("collapsible");
 var i;
 
 for (i = 0; i < coll.length; i++) {
-  coll[i].addEventListener("click", function() {
+  coll[i].addEventListener("click", function () {
+    for (item of coll) {
+      if (item != this) {
+        item.classList.remove("active");
+        item.nextElementSibling.style.display = "none";
+      }
+    }
+
     this.classList.toggle("active");
     var content = this.nextElementSibling;
     if (content.style.display === "block") {
       content.style.display = "none";
+      console.log("none");
     } else {
       content.style.display = "block";
+      console.log("block");
     }
   });
 }
