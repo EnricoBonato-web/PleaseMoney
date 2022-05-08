6a8197a259f59473b096eac059cd88ad2f4131f0
Enrico <enrico.bonato95@gmail.com>
2022-05-08 21:19:50 +0200

    MinorBugFix

--- js/scripts.js
+++ js/scripts.js
@@ -20,7 +20,7 @@ for (i = 0; i < coll.length; i++) {
     });
 }
 function copyToClipboard(element) {
-    var $temp = $("<input>");
+    let $temp = $("<input>");
     $("body").append($temp);
     $temp.val($(element).text()).select();
     document.execCommand("copy");
