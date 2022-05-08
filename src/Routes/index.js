13241f0654996e7fcf50f2f8433d5642a5e3aaaa
Enrico <enrico.bonato95@gmail.com>
2022-05-08 22:41:12 +0200

    minor

--- src/Routes/index.js
+++ src/Routes/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
+import '../public/index.html';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
